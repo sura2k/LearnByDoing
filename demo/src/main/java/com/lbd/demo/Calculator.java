e11662f9a3f0c665dd5f4e37ede8d46d0dedee21
Suranga <sura.btaccel@gmail.com>
2015-11-20 15:10:08 +0530

    removed conflicts

--- demo/src/main/java/com/lbd/demo/Calculator.java
+++ demo/src/main/java/com/lbd/demo/Calculator.java
@@ -6,22 +6,7 @@ public class Calculator {
 		return a+b;
 	}
 	
-<<<<<<< HEAD
 	public double substract(double a, double b){
 		return a-b;
 	}
-	
-=======
-	public double add(double a, double b){
-		return a+b;
-	}
-	
-	public int substract(int a, int b){
-		return a-b;
-	}
-	
-	public double substract(double a, double b){
-		return a-b;
-	}
->>>>>>> issue23
 }
